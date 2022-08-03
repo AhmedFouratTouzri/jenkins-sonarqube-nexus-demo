cdd55eb4abbece1d09d4ffbd387ccf4ec384d0e7
Ahmed Fourat TOUZRI <ahmedfourat.touzri@esprit.tn>
2022-08-03 22:03:32 +0100

    Force 200 OK code on '/' path

--- src/main/java/tn/esprit/spring/control/UserRestControl.java
+++ src/main/java/tn/esprit/spring/control/UserRestControl.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 
 import tn.esprit.spring.entities.User;
 import tn.esprit.spring.services.IUserService;
